65777f4bf2bd47ea97bf65bfee9537fe2be8084c
Ashok <ashokakumbar06@gmail.com>
2025-03-01 01:02:59 +0530

    N

--- src/main/java/com/java8/java8/Java8Application.java
+++ src/main/java/com/java8/java8/Java8Application.java
@@ -8,6 +8,11 @@ public class Java8Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Java8Application.class, args);
+		System.out.println("    A        SSSSS      H       H      OOOO      K      K  ");
+		System.out.println("   A A      S     S     H       H     O    O     K    K    ");
+		System.out.println("  AAAAA     S     s     H H H H H     O    O     K  K      ");
+		System.out.println(" A     A    S     S     H       H     O    O     K    K    ");
+		System.out.println("A       A    SSSSS      H       H      OOOO      K      K  ");
 	}
 
 }
