41aff7ff4fa132bd1d23f049347ec15b87b41caa
Ashok <ashokakumbar06@gmail.com>
2025-03-01 13:29:10 +0530

    new files

--- src/main/java/com/java8/java8/String/findStringL5.java
+++ src/main/java/com/java8/java8/String/findStringL5.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class findStringL5 {
 
     public static void main(String[] args) {
+
         List<String> list = List.of("ashoka", "ravi", "kary", "sucsses");
 
         list.stream().filter(e -> e.length() > 5).forEach(System.out::println);
